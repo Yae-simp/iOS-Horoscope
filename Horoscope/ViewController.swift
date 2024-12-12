d0e662975f25f75012ee43d298e6722f615f5194
tardes <irenexingni@tutanota.com>
2024-12-12 18:31:07 +0100

    Initial Commit

---
+++ Horoscope/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Horoscope
+//
+//  Created by Tardes on 12/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HoroscopeTests/HoroscopeTests.swift
@@ -0,0 +1,17 @@
+//
+//  HoroscopeTests.swift
+//  HoroscopeTests
+//
+//  Created by Tardes on 12/12/2024.
+//
+
+import Testing
+@testable import Horoscope
+
+struct HoroscopeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
