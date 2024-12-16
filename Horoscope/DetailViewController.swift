6501e470faf0e8cc10bfd1b77441cd0842d1dda9
tardes <irenexingni@tutanota.com>
2024-12-16 20:33:15 +0100

    api call works

--- Horoscope/DetailViewController.swift
+++ Horoscope/DetailViewController.swift
@@ -4,31 +4,73 @@
 //
 //  Created by Tardes on 13/12/2024.
 //
-
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
     var horoscope: Horoscope!
-
+    
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var datesLabel: UILabel!
-    @IBOutlet weak var dailyLabel: UILabel!
+    @IBOutlet weak var dailyTextView: UITextView!
     
-    var horoscopeId: String = "aries"
     var dailyResult: String = ""
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        navigationItem.title = horoscope.name
         
+        navigationItem.title = horoscope.name
         datesLabel.text = horoscope.dates
         iconImageView.image = horoscope.icon
-    }
-
-    func getDaily(method: String) {
+        getDaily(id: horoscope.id)
     }
     
+    func getDaily(id: String) {
+        let urlString = "https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign=\(id)&day=TODAY"
+    
+        guard let url = URL(string: urlString) else {
+            self.dailyResult = "Invalid URL"
+            self.updateUI()
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                self.dailyResult = "Request Error: \(error.localizedDescription)"
+                self.updateUI()
+                return
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode == 200 {
+                    guard let data = data else {
+                        self.dailyResult = "No data received"
+                        self.updateUI()
+                        return
+                    }
+                    if let jsonResponse = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                       let data = jsonResponse["data"] as? [String: Any],
+                       let horoscopeData = data["horoscope_data"] as? String {
+                        self.dailyResult = horoscopeData
+                    } else {
+                        self.dailyResult = "Failed to parse the horoscope data."
+                    }
+                } else {
+                    self.dailyResult = "HTTP Error: \(httpResponse.statusCode)"
+                }
+            } else {
+                self.dailyResult = "Invalid response"
+            }
+            self.updateUI()
+        }
+        task.resume()
+    }
+    
+    func updateUI() {
+        DispatchQueue.main.async {
+            self.dailyTextView.text = self.dailyResult
+        }
+    }
 }
+
+
